00ff63fb8359586eb3d5cde8808c8db81b91d3eb
BaadrHub <73551236+BaadrHub@users.noreply.github.com>
2020-11-16 11:55:20 +0000

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,73 @@
+
+let lessons = [
+    { 
+      subject: "Science",
+      location: "Hatchcroft Building Hendon Campus",
+      price: 150,
+      space: 5,
+      image: "../Images/Science.png"
+    },
+    { 
+       subject: "Geography",
+      location: "Ritterman Building - Hendon Campus",
+      price: 70,
+      space: 5,
+      image: "../Images/Geography.png"
+    },
+    { 
+        subject: "Spanish",
+       location: "Remote Learning ONLY",
+       price: 95,
+       space: 5,
+       image: "../Images/Spanish.png"
+     },
+     { 
+        subject: "Computing",
+       location: "College Building - Hendon Campus",
+       price: 65,
+       space: 5,
+       image: "../Images/Computing.png"
+     },
+     { 
+        subject: "Web Development",
+       location: "Remote Learning ONLY",
+       price: 50,
+       space: 5,
+       image: "../Images/WebDev.jpg"
+     },
+     { 
+        subject: "Augmented Reality",
+       location: "Poller Building",
+       price: 30,
+       space: 5,
+       image: "../Images/Augmented Reality.png"
+     },
+     { 
+        subject: "Dance",
+       location: "Hatchcroft Building",
+       price: 30,
+       space: 5,
+       image: "../Images/Dance.png"
+     },
+     { 
+        subject: "Acting",
+       location: "College Building",
+       price: 90,
+       space: 5,
+       image: "../Images/Acting.png"
+     },
+     { 
+        subject: "Economics",
+       location: "College Building",
+       price: 70,
+       space: 5,
+       image: "../Images/Economics.png"
+     },
+     { 
+        subject: "Politics",
+       location: "Politics Centre",
+       price: 85,
+       space: 5,
+       image: "../Images/Politics.png"
+     },
+  ]
